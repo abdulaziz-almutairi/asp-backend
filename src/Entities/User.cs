6c6bc3e1bb36cd591d000fefbdea56d5c51bab09
win lap <faisl4465@gmail.com>
2024-04-28 17:18:37 +0300

    added implemented user.cs

--- src/Entities/User.cs
+++ src/Entities/User.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace sda_onsite_2_csharp_backend_teamwork.src.Entities
-{
-    public class User
-    {
+namespace sda_onsite_2_csharp_backend_teamwork.src.Entities;
 
+public class User
+{
+    public User(string id, string first_name, string last_name, string email, string password, int salt, string role)
+    {
+        Id = id;
+        FirstName = first_name;
+        LastName = last_name;
+        Email = email;
+        Password = password;
+        Salt = salt;
+        Role = role;
     }
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public int Salt { get; set; }
+    public string Role { get; set; }
 }
