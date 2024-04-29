6c72f3e4e21f2ca3f54824b2349eaade9bd72d4f
win lap <faisl4465@gmail.com>
2024-04-29 14:09:30 +0300

    fixed id in test data

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -11,9 +11,9 @@ public class DataBaseContext
     {
         users = [
             new User("1", "Faisal", "Mohammed", "faisal@faisal.com", "123456", 21, "Admin"),
-            new User("1", "Sattam", "Ahmed", "Sattam@faisal.com", "112233", 18, "Costumer"),
-            new User("1", "Abdulaziz", "Fathe", "Abdulaziz@faisal.com", "asdfgh", 03, "Costumer"),
-            new User("1", "Turki", "Omar", "Turki@faisal.com", "password1", 72, "Costumer")
+            new User("2", "Sattam", "Ahmed", "Sattam@faisal.com", "112233", 18, "Costumer"),
+            new User("3", "Abdulaziz", "Fathe", "Abdulaziz@faisal.com", "asdfgh", 03, "Costumer"),
+            new User("4", "Turki", "Omar", "Turki@faisal.com", "password1", 72, "Costumer")
         ];
     }
 }
