586a288bce31a2defd34695bd625e112a61be1f0
Abdulaziz-Almutairi <aziz1418@outlook.sa>
2024-04-30 11:06:14 +0300

    Adds a scoped service

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,8 @@
 
 using sda_backend_teamwork.src.Abstractions;
 using sda_backend_teamwork.src.Controllers;
+using sda_onsite_2_csharp_backend_teamwork;
+using sda_onsite_2_csharp_backend_teamwork.src.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +17,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IProductService, ProductService>(); //this is the built-in DI container for the Service
 builder.Services.AddScoped<IProductRepository, ProductRepository>(); //this is the built-in DI container for the Repository
+
+builder.Services.AddScoped<ICustomerOrderRepository, CustomerOrderRepository>();
 
 
 var app = builder.Build();
