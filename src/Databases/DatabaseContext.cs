868ddbb96219c21a5f6c6149b124248856ebf438
Abdulaziz-Almutairi <aziz1418@outlook.sa>
2024-05-01 17:12:15 +0300

    Database and appsetting file

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -4,31 +4,31 @@ using sda_onsite_2_csharp_backend_teamwork.src.Entities;
 namespace sda_onsite_2_csharp_backend_teamwork.src.Databases;
 public class DatabaseContext : DbContext
 {
-    public List<Product> Products { get; set; }
-    // public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<User> Users { get; set; }
     public DbSet<CustomerOrder> CustomerOrder { get; set; }
 
-    public DatabaseContext()
-    {
-        Products = [
-        new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
-        new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
-        new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
-    ];
-        //     users = [
-        //     new User("1", "Faisal", "Mohammed", "faisal@faisal.com", "123456", 21, "Admin"),
-        //     new User("2", "Sattam", "Ahmed", "Sattam@faisal.com", "112233", 18, "Costumer"),
-        //     new User("3", "Abdulaziz", "Fathe", "Abdulaziz@faisal.com", "asdfgh", 03, "Costumer"),
-        //     new User("4", "Turki", "Omar", "Turki@faisal.com", "password1", 72, "Costumer")
-        // ];
-
-    }
-
-    // private IConfiguration _config;
-    // public DatabaseContext(IConfiguration config)
+    // public DatabaseContext()
     // {
-    //     _config = config;
+    //     Products = [
+    //     new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
+    //     new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
+    //     new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
+    // ];
+    //     users = [
+    //     new User("1", "Faisal", "Mohammed", "faisal@faisal.com", "123456", 21, "Admin"),
+    //     new User("2", "Sattam", "Ahmed", "Sattam@faisal.com", "112233", 18, "Costumer"),
+    //     new User("3", "Abdulaziz", "Fathe", "Abdulaziz@faisal.com", "asdfgh", 03, "Costumer"),
+    //     new User("4", "Turki", "Omar", "Turki@faisal.com", "password1", 72, "Costumer")
+    // ];
+
     // }
+
+    private IConfiguration _config;
+    public DatabaseContext(IConfiguration config)
+    {
+        _config = config;
+    }
 
 
 
@@ -37,8 +37,10 @@ public class DatabaseContext : DbContext
     @"Host=localhost;Username=Root;Password=Password;Datebase=ecommerce"
     */
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql(@"Host=localhost;Username=postgres;Password=QQAAZZ12341234;Datebase=ecommerce");
+        => optionsBuilder.UseNpgsql(@$"Host={_config["Db:Host"]};Username={_config["Db:Username"]};Password={_config["Db:Password"]};Datebase={_config["Db:Database"]}");
 
 }
+
+
 
 
