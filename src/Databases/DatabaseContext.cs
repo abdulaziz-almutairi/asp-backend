363eb74cc69bf693582599b04ff69b4d9b16df67
Abdulaziz-Almutairi <aziz1418@outlook.sa>
2024-05-05 11:40:49 +0300

    Useing Sneak Cae

--- src/Databases/DatabaseContext.cs
+++ src/Databases/DatabaseContext.cs
@@ -8,21 +8,6 @@ public class DatabaseContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<CustomerOrder> CustomerOrder { get; set; }
 
-    // public DatabaseContext()
-    // {
-    //     Products = [
-    //     new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
-    //     new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
-    //     new Product(new Guid(), "iPhone 6s", DateTime.Now, 100, "Phone"),
-    // ];
-    // //     users = [
-    // //     new User("1", "Faisal", "Mohammed", "faisal@faisal.com", "123456", 21, "Admin"),
-    // //     new User("2", "Sattam", "Ahmed", "Sattam@faisal.com", "112233", 18, "Costumer"),
-    // //     new User("3", "Abdulaziz", "Fathe", "Abdulaziz@faisal.com", "asdfgh", 03, "Costumer"),
-    // //     new User("4", "Turki", "Omar", "Turki@faisal.com", "password1", 72, "Costumer")
-    // // ];
-
-    // }
 
     private IConfiguration _config;
     public DatabaseContext(IConfiguration config)
@@ -33,11 +18,11 @@ public class DatabaseContext : DbContext
 
 
     /*
-    Please note that u nead to add ur own connection string in the Username and Password Only for example :
-    @"Host=localhost;Username=Root;Password=Password;Datebase=ecommerce"
+    Please Note u need to cheange the Pgadmin Password on appsettings.json file 
     */
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql(@$"Host={_config["Db:Host"]};Username={_config["Db:Username"]};Password={_config["Db:Password"]};Datebase={_config["Db:Database"]}");
+        => optionsBuilder.UseNpgsql(@$"Host={_config["Db:Host"]};Username={_config["Db:Username"]};Password={_config["Db:Password"]};Datebase={_config["Db:Database"]}")
+        .UseSnakeCaseNamingConvention();
 
 }
 
